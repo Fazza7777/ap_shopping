c181eff7c2f60a987741689de023f88d94a326ca
Fazza7777 <aungbarlaay7777@gmail.com>
2020-09-20 03:48:38 -0700

    auth

--- admin/index.php
+++ admin/index.php
@@ -21,11 +21,11 @@ if(empty($_POST['search'])){
      
 <div class="content">
 <div class="container-fluid">
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-header">
-<h3 class="card-title">Product Listing</h3>
+    <div class="row">
+        <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+            <h3 class="card-title">Product Listing</h3>
 </div>
 
 <div class="card-body">
